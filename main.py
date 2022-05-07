b9b9bdae298e3b8f3b1b40d33023fd8554881f2e
apostolistriantafyllou <67221448+apostolistriantafyllou@users.noreply.github.com>
2022-05-07 20:37:54 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from win10toast_click import ToastNotifier
+from ast import literal_eval
+import requests, time, webbrowser
+
+#default port is 3000
+apiurl = "example.com:port"
+
+def onclick(url):
+	webbrowser.open(url, new=0, autoraise=True)
+
+while True:
+	r = requests.get('http://' + apiurl + '/pi4-8gb-stockcheck/[4,8]')
+	if r.text != 'null':
+		pilist = literal_eval(r.text)
+		for res in pilist:
+			notification = ToastNotifier()
+			notification.show_toast(
+				title = "Raspberry Pi 4 8gb in stock!!!! price: " + res[1],
+				msg = "url: " + res[0],
+				threaded=True,
+				duration=60,
+				callback_on_click = lambda: onclick(res[0]),
+				icon_path = './raspi.ico'
+			)
+	time.sleep(60)
